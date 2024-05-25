7b969f36ca2b39279743cc08dc50c681a8ff474f
MateusOK <mateusribeiro-2004@hotmail.com>
2024-05-25 17:30:19 -0300

    feat: load and preprocess the image

--- classify_a_image.py
+++ classify_a_image.py
@@ -1,3 +1,14 @@
 from keras.api.models import load_model
+from keras.api.preprocessing import image
+import numpy as np
+from keras.api.applications.vgg16 import preprocess_input
 
 model = load_model("path_to_model_trained")
+
+img_path = "path_to_image_to_analize"
+
+img_height, img_width = 224, 224
+img = image.load_img(img_path, target_size=(img_height, img_width))
+img_array = image.img_to_array(img)
+img_array = np.expand_dims(img_array, axis=0)
+img_array = preprocess_input(img_array)
